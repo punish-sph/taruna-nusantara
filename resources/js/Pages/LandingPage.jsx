cfdc9258c677bee752739bf570b0d844f55ae6f1
Ferdiansyah <Psocietyyy@gmail.com>
2025-07-13 11:24:31 -0700

    integasiin inertia

---
+++ resources/js/Pages/LandingPage.jsx
@@ -0,0 +1,7 @@
+export default function LandingPage() {
+    return (
+        <div className="bg-gray-50">
+            <h1 className="text-3xl font-semibold underline">Hello</h1>
+        </div>
+    );
+}
